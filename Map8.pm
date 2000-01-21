90adbefb44ffad90a15baecf7c483f0d9312e801
Gisle Aas <gisle@ActiveState.com>
2000-01-21 10:54:18 +0000

    Import of GAAS/Unicode-Map8-0.09 from CPAN.
    gitpan-cpan-distribution: Unicode-Map8
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         GAAS/Unicode-Map8-0.09.tar.gz
    gitpan-cpan-author:       GAAS
    gitpan-cpan-maturity:     released

--- Map8.pm
+++ Map8.pm
@@ -15,7 +15,7 @@ require Exporter;
 *import = \&Exporter::import;
 @EXPORT_OK = qw(NOCHAR MAP8_BINFILE_MAGIC_HI MAP8_BINFILE_MAGIC_LO);
 
-$VERSION = '0.08';  # $Id: Map8.pm,v 1.22 1999/09/14 12:13:13 gisle Exp $
+$VERSION = '0.09';  # $Id: Map8.pm,v 1.22 1999/09/14 12:13:13 gisle Exp $
 #$DEBUG++;
 
 bootstrap Unicode::Map8 $VERSION;
